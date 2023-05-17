a87f8cc2378591e2953821d0b3b67999a6c8b6f9
Muhammad Fadhil Hidayatullah <103361348+fadhil30@users.noreply.github.com>
2023-05-17 14:22:31 +0700

    Add files via upload

---
+++ lib/models/todo_model.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
+
+enum Filter {
+  all,
+  active,
+  completed,
+}
+
+Uuid uuid = Uuid();
+
+class todo extends Equatable{
+  final String id;
+  final String desc;
+  final bool completed;
+
+  todo({
+    String? id,
+    required this.desc,
+    this.completed = false,
+  }) : this.id = id ?? uuid.v4();
+
+@override
+List<Object?> get props => [id, desc, completed];
+
+
+@override
+String toString() {
+    return 'todo{id=$id, desc=$desc, completed=$completed}';
+  }
+}
